a29ef082fc8fbfb3cfcc43f0443f77cbd56b8de0
dup <58719567@qq.com>
2018-07-12 09:13:32 +0800

    use exception

--- demo/src/main/java/com/dup/demo/controller/UserController.java
+++ demo/src/main/java/com/dup/demo/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+ 
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
